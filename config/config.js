7f7d50a50aab8a9611889994dd191070881d8601
theblackbeard <ccarvalhos@outlook.com>
2016-10-13 17:50:00 -0300

    OpenShift

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
     'IPSERVER' : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     'PORTSERVER': process.env.OPENSHIFT_NODEJS_PORT || 8001,
-    'MONGO': process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/',
+    'MONGO': process.env.MONGODB_URL, //|| 'mongodb://127.0.0.1:27017/',
     'MONGODB': 'movies',
     'ROOT' : path.join(__dirname, '..')
 }
